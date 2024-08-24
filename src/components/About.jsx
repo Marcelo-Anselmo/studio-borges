191f3c382fd3d3fafa19c347616c48657487c442
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-08-24 13:44:37 -0300

    About Updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
 const About = () => {
   return (
-    <div className="flex flex-col py-16 text-black space-x-8 w-full h-full">
-      <div className="text-center w-2/3 h-full mb-4">
-        <h2 className="text-3xl pl-24 font-semibold font-serif">Sobre Nós</h2>
+    <div className="flex flex-col py-24 text-black space-x-8 w-full h-full overflow-x-hidden">
+      <div className="text-center md:w-2/5 2xl:w-3/5 h-full mb-4">
+        <h2 className="text-4xl pl-24 font-semibold font-serif">Sobre Nós</h2>
       </div>
-      <div className="flex flex-row justify-center space-x-8 w-full h-full">
+      <div className="flex flex-row justify-center space-x-28 w-full h-full">
         <div>
-          <h3 className="text-xl font-semibold font-serif py-4">Nossa Missão</h3>
-          <p className="font-normal text-gray-500">
+          <h3 className="text-xl font-semibold font-serif py-4">
+            Nossa Missão
+          </h3>
+          <p className="font-normal text-gray-500 text-base">
             Empoderar mulheres afro com <br />
             cuidados capilares <br />
             personalizados e <br />
@@ -15,8 +17,10 @@ const About = () => {
           </p>
         </div>
         <div>
-          <h3 className="text-xl font-semibold font-serif py-4">Nossa Equipe</h3>
-          <p className="font-normal text-gray-500">
+          <h3 className="text-xl font-semibold font-serif py-4">
+            Nossa Equipe
+          </h3>
+          <p className="font-normal text-gray-500 text-base">
             Profissionais altamente <br />
             qualificados, com anos de <br />
             experiência no tratamento de <br />
@@ -24,8 +28,10 @@ const About = () => {
           </p>
         </div>
         <div>
-          <h3 className="text-xl font-semibold font-serif py-4">Nossa História</h3>
-          <p className="font-normal text-gray-500">
+          <h3 className="text-xl font-semibold font-serif py-4">
+            Nossa História
+          </h3>
+          <p className="font-normal text-gray-500 text-base">
             Fundado em 2010, o Studio <br />
             Borges se tornou referência em <br />
             cuidados capilares <br />
