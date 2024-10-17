308bf75aa886ed25194bf5794b0a0da7f55130fd
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2024-10-17 16:55:51 -0300

    try_fix_imgs_loading

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -9,22 +9,22 @@ const Photos = () => {
       <div className="flex flex-col md:flex-row justify-center gap-8">
         <CardsPh
           title="Elegância Atemporal"
-          src="./src/imgs/pago_2.jpg"
+          src="../src/imgs/pago_2.jpg"
           p="Penteados sofisticados que valorizam sua beleza natural."
         />
         <CardsPh
           title="Estilo Arrojado"
-          src="./src/imgs/pago_1.jpg"
+          src="../src/imgs/pago_1.jpg"
           p="Transformações ousadas que elevam sua autoestima."
         />
         <CardsPh
           title="Cuidado Dedicado"
-          src="./src/imgs/pago_3.jpg"
+          src="../src/imgs/pago_3.jpg"
           p="Tratamentos exclusivos que nutrem e revitalizam seus cabelos."
         />
         <CardsPh
           title="Versatilidade Única"
-          src="./src/imgs/img_1.jpg"
+          src="../src/imgs/img_1.jpg"
           p="Diversas opções de cortes e estilos para realçar sua beleza."
         />
       </div>
